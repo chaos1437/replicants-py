1f8c65895bbf75c144cb26b93ec702d8542e79e4
chaos1437 <chaoslord1437@gmail.com>
2024-08-20 13:22:19 +0300

    feat: walking behavior in execute_interaction

--- world.py
+++ world.py
@@ -1,15 +1,15 @@
-from replicant import Bot
 from world_utils import *
 
 
 
 class World:
-    def __init__(self, width, height, tick=0):
+    def __init__(self, width, height, world_map, tick=0):
         self.width = width
         self.height = height
         self.bots = []
         self.pending_interactions = []
         self.tick = tick
+        self.map = world_map
 
 
     def queue_interaction(self, interaction: Interaction):
@@ -25,8 +25,27 @@ class World:
 
 
     def execute_interaction(self, interaction: Interaction):
-
+        
         match interaction.type:
-            case -1:
-                pass
+            
+            case -1: # Death
+                cell = map[interaction.bot.x][interaction.bot.y]
+                cell.contains = None
+                cell.energy += interaction.bot.energy // 2
+            
+            case 0: # Move
 
+                match interaction.direction:
+                    case 0: #left
+                        self.map.move(interaction.bot.x, interaction.bot.y,  interaction.bot.x - 1, interaction.bot.y)
+                    
+                    case 1: #up
+                        self.map.move(interaction.bot.x, interaction.bot.y,  interaction.bot.x, interaction.bot.y + 1)
+                    
+                    case 2: #right
+                        self.map.move(interaction.bot.x, interaction.bot.y,  interaction.bot.x + 1, interaction.bot.y)
+                    
+                    case 3: #down
+                        self.map.move(interaction.bot.x, interaction.bot.y,  interaction.bot.x, interaction.bot.y - 1)
+
+                
